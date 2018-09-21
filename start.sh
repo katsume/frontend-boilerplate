0106b183cbdf7853131ccb072dcc4718e67ad12d
Yuichiro Katsume <y@katsu.me>
2018-09-21 23:40:51 +0900

    Stop docker container when script terminates

--- start.sh
+++ start.sh
@@ -7,4 +7,6 @@ if ! docker ps | grep $NAME > /dev/null ; then
 	docker run -d --rm --name $NAME -p 8000:80 -v $(cd build && pwd):/usr/share/nginx/html/ nginx:alpine
 fi
 
+trap 'docker stop $NAME' EXIT
+
 gulp
